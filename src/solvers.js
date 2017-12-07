62998e29c21e2324f26cc47a5b90b8688a80ec5e
decross1 <decross1@me.com>
2017-12-06 20:32:03 -0800

    Minor changes, commiting additional psuedocode

--- src/solvers.js
+++ src/solvers.js
@@ -18,10 +18,17 @@
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
   // our code here
-  
+  // build unpopulated board
+  // for loop on length of n
+  // each iteration adds a rook at row[i], col[i]
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
+  // two arrays, illegalRow & illegalCol (illegalMaj illegalMin for Queen?) *ADVANCED*
+  // each time we place a rook we add the row and col to those arrays and skip them in future searches **
+  // BASIC: skip positions we've iterated with on previous loops (set row/col of first loop
+  // to current position + 1 on successful placement of rook)
+  // QUEENS: if we fail to place a queen on a row, immediately iterate original loop to next position
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
